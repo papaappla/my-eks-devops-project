c1381b39c5caa5ac2e47a8b7bef35a3ede1a6249
jihoonP <papaappla@gmail.com>
2026-03-24 14:29:28 +0900

    image debug

--- eks.tf
+++ eks.tf
@@ -58,7 +58,7 @@ resource "aws_eks_node_group" "main" {
     min_size     = 1
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
